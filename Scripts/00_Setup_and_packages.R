b5535170167ae2ccc972db5fe30a1cd5da7b9d7d
Tom Prendergast (tier0) <tier0\tom.prendergast@ec2amaz-9u4wpc.tier0.prd.thf.aws>
2023-10-03 12:37:40 +0000

    Initial draft of SitRep webscraper

---
+++ Scripts/00_Setup_and_packages.R
@@ -0,0 +1,30 @@
+## Install and load packages
+
+packages <- c('tidyverse', 'readxl', 'rvest', 'here')
+
+installed_packages <- packages %in% row.names(installed.packages())
+
+if (any(installed_packages == FALSE)){
+  install.packages(packages[!installed_packages])
+}
+
+lapply(packages, library, character.only = TRUE)
+
+
+## Set up folder structure
+
+# Parent folders
+
+ifelse(!dir.exists(file.path(here('Raw_data/'))), dir.create(file.path(here('Raw_data/'))), print('Raw data directory already exists'))  
+
+ifelse(!dir.exists(file.path(here('Outputs/'))), dir.create(file.path(here('Outputs/'))), print('Outputs directory already exists'))
+
+# Sub folders
+
+data_subfolders <- c('Acute_SitRep_data', 'Community_SitRep_data', 'CSDS_data', 'ASC_data')
+
+lapply(data_subfolders, function(i){
+  ifelse(!dir.exists(file.path(here('Raw_data/', i))), dir.create(file.path(here('Raw_data/', i))), print('Directory already exists'))
+})
+
+
